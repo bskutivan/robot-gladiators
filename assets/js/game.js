188a0bf178ff3d304d3ba78eaebdb74d0bee95dd
BrandonKutivan <bskutivan@gmail.com>
2020-03-24 18:38:34 -0700

    Start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,7 @@ var playerAttack = 10;
 var playerMoney = 10;
 
 console.log(playerName, playerAttack, playerHealth);
-//Enemy array created with []!!
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"]
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
@@ -73,6 +72,56 @@ var fight = function(enemyName) {
     }
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i])
-}
+//Display player's score in alert
+
+//After defeating an enemy ask if they want to visit the shop
+
+//If so be presented with refilling health, upgrading attack, or leaving the store.
+// START GAME FUNCTION
+var startGame = function() {
+    for(var i = 0; i < enemyNames.length; i++) {
+        playerHealth = 100;
+        playerAttack = 10;
+        playerMoney = 10;
+
+        if (playerHealth > 0) {
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+            var pickedEnemyName = enemyNames[i];
+
+            enemyHealth = 50;
+
+            fight(pickedEnemyName);
+        }
+
+        else {
+            window.alert("You have lost your robot in battle! Game Over!");
+            break;
+        }
+    }
+    //After loop ends, player is either out of health or enemies to fight
+    endGame();
+};
+
+// END GAME FUNCTION
+var endGame = function() {
+    window.alert("The game has now ended. Let's see how you did!");
+
+    if (playerHealth > 0) {
+        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney +".");
+    }
+    else {
+        window.alert("You've lost your robot in battle.");
+    }
+
+    var playerAgainConfirm = window.confirm("Would you like to play again?");
+
+    if (playerAgainConfirm) {
+        startGame();
+    }
+    else {
+        window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+    }
+};
+
+startGame();
